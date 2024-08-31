b4abfb880285efc96cb8e3b98c88c96c66901d2d
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-08-31 14:54:47 +0900

    Support enable GPU (#27)

--- scripts/disable_gpu.sh
+++ scripts/disable_gpu.sh
@@ -3,11 +3,50 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 DISTROS=$(ls -d ${SCRIPT_DIR}/../*/ | sed 's|'${SCRIPT_DIR}\/..\/'||g' | sed 's/\///g')
 
-for distro in ${DISTROS[@]}; do
-    if [[ ${distro} != "scripts" ]]; then
-        sed -i '/deploy/,+6d' ${SCRIPT_DIR}/../${distro}/docker-compose.yml
-    fi
-done
+function disable_gpu() {
+    for distro in ${DISTROS[@]}; do
+        if [[ ${distro} != "scripts" ]]; then
+            sed -i '/deploy/,+6d' ${SCRIPT_DIR}/../${distro}/docker-compose.yml
+        fi
+    done
 
-echo ""
-echo "Disabled GPU"
+    echo ""
+    echo "Disabled GPU"
+    echo ""
+    echo -e "\e[33m(If you want to enable GPU, please run '$0 enable_gpu')\e[m"
+}
+
+function enable_gpu() {
+    local target_string="\\
+\    deploy:\\
+      resources:\\
+        reservations:\\
+          devices:\\
+            - driver: nvidia\\
+              count: 1\\
+              capabilities: [ gpu ]"
+
+    for distro in ${DISTROS[@]}; do
+        if [[ ${distro} != "scripts" ]] && [[ ${distro} != "alpine3.17" ]]; then
+            local target_file=${SCRIPT_DIR}/../${distro}/docker-compose.yml
+            local count=$(grep -c "deploy" ${target_file})
+            if [[ ${count} -eq 0 ]]; then
+                local target_line=$(grep -n "command" ${target_file} | cut -d ":" -f 1 | head -n 1)
+                sed -i "${target_line}a ${target_string}" ${target_file}
+            fi
+        fi
+    done
+
+    echo ""
+    echo "Enabled GPU"
+}
+
+function main() {
+    if [[ $# -eq 0 ]]; then
+        disable_gpu
+    elif [[ $1 == "enable_gpu" ]]; then
+        enable_gpu
+    fi
+}
+
+main $@
